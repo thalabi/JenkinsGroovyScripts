babdcb8fb0ec9a743edeaa34f23c573127ee75cd
Jenkins Automation Server <jenkins@pa28.kerneldc.com>
2020-04-12 16:11:02 -0400

    Add retrieval of NEXUS3_DEPLOYMENT_USERNAME env var

--- artifactItemList.groovy
+++ artifactItemList.groovy
@@ -1,4 +1,6 @@
 import groovy.json.JsonSlurper
+import hudson.slaves.EnvironmentVariablesNodeProperty
+
 println ("artifactItemList begin")
 println ("binding: " + binding)
 def Map<String, String> bindingMap = binding.getVariables()
@@ -6,17 +8,21 @@ bindingMap.each{entry ->
 	println ("binding key: " + entry.key + ", binding value: " + entry.value)
 }
 def jenkins = binding.getVariable("jenkins")
-println ("jenkins: " + jenkins);
-
-def deploymentPassword = getPassword("nexus3_deployment")
+def globalNodeProperties = jenkins.getGlobalNodeProperties()
+def envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class);
+def envVars = envVarsNodePropertyList.get(0).getEnvVars();
+println ("envVars: " + envVars);
+def deploymentUsername = envVars.get("NEXUS3_DEPLOYMENT_USERNAME")
+println ("deploymentUsername: " + deploymentUsername)
+def deploymentPassword = getPassword(deploymentUsername)
 //println deploymentPassword
 
 try {
     List<String> artifacts = new ArrayList<String>()
     def releasesArtifactsUrl = "http://localhost:8081/service/rest/v1/components?repository=maven-releases"          
     def snapshotsArtifactsUrl = "http://localhost:8081/service/rest/v1/components?repository=maven-snapshots"          
-    def releasesArtifactsJson = ["curl", "-s", "-u", "deployment:"+deploymentPassword, "-H", "accept: application/json", "-k", "--url", "${releasesArtifactsUrl }"].execute().text
-    def snapshotsArtifactsJson = ["curl", "-s", "-u", "deployment:"+deploymentPassword, "-H", "accept: application/json", "-k", "--url", "${snapshotsArtifactsUrl }"].execute().text
+    def releasesArtifactsJson = ["curl", "-s", "-u", deploymentUsername+":"+deploymentPassword, "-H", "accept: application/json", "-k", "--url", "${releasesArtifactsUrl }"].execute().text
+    def snapshotsArtifactsJson = ["curl", "-s", "-u", deploymentUsername+":"+deploymentPassword, "-H", "accept: application/json", "-k", "--url", "${snapshotsArtifactsUrl }"].execute().text
 
     // parse releases json
     def releasesArtifacts = parseAndGetArtifactList(releasesArtifactsJson, "FlightLogServer", true)
@@ -60,9 +66,9 @@ def getPassword(username) {
             'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
             ).first()
 
-      def password = systemCredentialsProvider.credentials.first().password
+        def password = systemCredentialsProvider.credentials.first().password
 
-	  return password.getPlainText()
+	    return password.getPlainText()
 
 
     } else {
